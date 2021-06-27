89138405a6ae7ec9389867c2a93e3751bf9cea5f
larryz1230 <larryzhi1230@gmail.com>
2021-06-26 21:10:41 -0700

    finished store activity

--- app/src/main/java/com/example/runningapp/StoreActivity.java
+++ app/src/main/java/com/example/runningapp/StoreActivity.java
@@ -2,8 +2,10 @@ package com.example.runningapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -32,6 +34,7 @@ public class StoreActivity extends AppCompatActivity {
     TextView storeTitle, creditText;
     Button b1, b2, b3, b4, b5, b6, b7, b8;
     ImageView sItem1; //idk if we need imageview
+    private static String URL_ADD = LoginAcitivty.ngrokID+"/RunningApp/addplace.php";
     int creditsss;
 
     @Override
@@ -59,6 +62,61 @@ public class StoreActivity extends AppCompatActivity {
         b7 = findViewById(R.id.sButton7);
         b8 = findViewById(R.id.sButton8);
 
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (creditsss<4000){
+                    Toast.makeText(StoreActivity.this, "Not enough credits", Toast.LENGTH_SHORT).show();
+                } else {
+                    add(4000);
+                }
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (creditsss<500){
+                    Toast.makeText(StoreActivity.this, "Not enough credits", Toast.LENGTH_SHORT).show();
+                } else {
+                    add(500);
+                }
+            }
+        });
+
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (creditsss<800){
+                    Toast.makeText(StoreActivity.this, "Not enough credits", Toast.LENGTH_SHORT).show();
+                } else {
+                    add(800);
+                }
+            }
+        });
+
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (creditsss<2000){
+                    Toast.makeText(StoreActivity.this, "Not enough credits", Toast.LENGTH_SHORT).show();
+                } else {
+                    add(2000);
+                }
+            }
+        });
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (creditsss<5000){
+                    Toast.makeText(StoreActivity.this, "Not enough credits", Toast.LENGTH_SHORT).show();
+                } else {
+                    add(5000);
+                }
+            }
+        });
+
+
 
 
 //        bItem1.setOnClickListener(new View.OnClickListener() {
@@ -75,6 +133,70 @@ public class StoreActivity extends AppCompatActivity {
 
     }
 
+
+    private void add(int sub){
+//        mRegister.setVisibility(View.GONE);
+
+
+
+        final Double distance = LoginAcitivty.user.getDistance();
+        final Integer credits = LoginAcitivty.user.getCredits()-sub;
+        final Integer id = LoginAcitivty.user.getId();
+
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_ADD,
+                new com.android.volley.Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
+
+
+
+                            if (success.equals("1")){
+                                Toast.makeText(StoreActivity.this, "Congratulations, you have bought an item", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(), MenuActivity.class));
+
+                            }
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Log.e("anyText",response);
+                            System.out.println(e.toString());
+                            Toast.makeText(StoreActivity.this, "Error" + e.toString(), Toast.LENGTH_SHORT).show();
+//                            mRegister.setVisibility(View.VISIBLE);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println(error.toString());
+                        Toast.makeText(StoreActivity.this, "Error" + error.toString(), Toast.LENGTH_SHORT).show();
+//                        mRegister.setVisibility(View.VISIBLE);
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map <String, String> params = new HashMap<>();
+                params.put ("id", id.toString());
+                params.put ("credits", credits.toString());
+                params.put("distance", distance.toString());
+
+                System.out.println(distance);
+                return params;
+
+
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
 
 
 }
